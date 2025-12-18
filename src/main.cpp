5591326c2d9c5c54aeae71c85defca65b7b8f9c7
AnishDikxit <anish.22053@gmail.com>
2025-12-18 18:42:06 +0530

    Testing for multiple PING

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,17 @@ int main(int argc, char **argv) {
   int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   const char* response = "+PONG\r\n";
-  send(client_fd, response, strlen(response), 0);
+  // send(client_fd, response, strlen(response), 0);
+  char buffer[1024] = {0};
+  int bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+  if(bytes_received<0){
+    std::cerr << "failed to read\n";
+    return 1;
+  }
+  std::string req(buffer);
+  if(req.find("PING")!=std::string::npos){
+    send(client_fd, response, strlen(response),0);
+  }
   close(client_fd);
   close(server_fd);
 
